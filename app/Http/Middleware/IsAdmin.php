bd1fcb2d4ba948b82bd4e9c884324191d8cde3a8
Amaury Barrois <amaury.brs70@outlook.com>
2025-12-19 09:23:54 +0100

    ajout middleware admin

---
+++ app/Http/Middleware/IsAdmin.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Http\Request;
+use Symfony\Component\HttpFoundation\Response;
+
+class IsAdmin
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Closure(\Illuminate\Http\Request): (\Symfony\Component\HttpFoundation\Response)  $next
+     */
+    public function handle(Request $request, Closure $next): Response
+    {
+        if (auth()->user() && auth()->user()->est_admin) { 
+        return $next($request);
+    }
+     return redirect()->route('A MODIFIER POUR METTRE PAGE DE LOGIN OU PAGE ACCUEIL !!! ')->with('error', "Vous n'avez pas accès à cette section."); 
+}
