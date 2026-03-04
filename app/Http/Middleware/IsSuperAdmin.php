9ee7dd1256f324b90b9054480aa71b317a4d1b7c
Anis KEHAL <kehalanis25@gmail.com>
2026-03-04 09:43:23 +0100

    IsSuperAdmin

---
+++ app/Http/Middleware/IsSuperAdmin.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Http\Request;
+use Symfony\Component\HttpFoundation\Response;
+
+class IsSuperAdmin
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Closure(\Illuminate\Http\Request): (\Symfony\Component\HttpFoundation\Response)  $next
+     */
+    public function handle(Request $request, Closure $next): Response
+    {
+        $user = auth()->user();
+
+        if ($user && $user->est_super_admin) {
+            return $next($request);
+        }
+
+        return redirect()
+            ->route('login')
+            ->with('error', "Vous n'avez pas accès à cette section.");
+    }
+}
