4d44c1a6ac5bfe89209a3e2cc7d9a2f0f9f41ae1
Anis KEHAL <kehalanis25@gmail.com>
2026-03-05 14:19:30 +0100

    no message

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -30,8 +30,8 @@ class UserFactory extends Factory
             'email' => fake()->unique()->safeEmail(),
             'email_verified_at' => now(),
             'password' => static::$password ??= Hash::make('password'),
-            'est_admin' => false,
-            'est_super_admin' => false,
+            'is_admin' => false,
+            'is_superadmin' => false,
             'remember_token' => Str::random(10),
         ];
     }
