b96dc9d2b8e0477540fbe353a9104516736da82a
Anis KEHAL <kehalanis25@gmail.com>
2026-03-06 09:21:39 +0100

    page login + register fonctionnel

--- resources/views/auth/forgot-password.blade.php
+++ resources/views/auth/forgot-password.blade.php
@@ -1,25 +1,106 @@
-<x-guest-layout>
-    <div class="mb-4 text-sm text-gray-600 dark:text-gray-400">
-        {{ __('Forgot your password? No problem. Just let us know your email address and we will email you a password reset link that will allow you to choose a new one.') }}
+<!DOCTYPE html>
+<html lang="fr">
+
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="">
+    <meta name="author" content="">
+
+    <title>Mot de passe oublié</title>
+
+    <!-- Fonts / CSS SB Admin 2 -->
+    <link href="{{ asset('sbadmin2/vendor/fontawesome-free/css/all.min.css') }}" rel="stylesheet" type="text/css">
+    <link
+        href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i"
+        rel="stylesheet">
+    <link href="{{ asset('sbadmin2/css/sb-admin-2.min.css') }}" rel="stylesheet">
+</head>
+
+<body class="bg-gradient-primary">
+
+    <div class="container">
+        <div class="row justify-content-center">
+            <div class="col-xl-10 col-lg-12 col-md-9">
+
+                <div class="card o-hidden border-0 shadow-lg my-5">
+                    <div class="card-body p-0">
+                        <div class="row">
+                            <!-- Colonne image / déco -->
+                            <div class="col-lg-6 d-none d-lg-block"
+                                 style="background: linear-gradient(135deg, rgba(78,115,223,.9), rgba(34,74,190,.9));">
+                            </div>
+
+                            <!-- Colonne formulaire -->
+                            <div class="col-lg-6">
+                                <div class="p-5">
+                                    <div class="text-center mb-4">
+                                        <h1 class="h4 text-gray-900 mb-2">Mot de passe oublié</h1>
+                                        <p class="text-muted mb-0">
+                                            Indiquez votre adresse email pour recevoir un lien de réinitialisation.
+                                        </p>
+                                    </div>
+
+                                    @if (session('status'))
+                                        <div class="alert alert-success">
+                                            {{ session('status') }}
+                                        </div>
+                                    @endif
+
+                                    <form class="user" method="POST" action="{{ route('password.email') }}">
+                                        @csrf
+
+                                        <div class="form-group">
+                                            <input
+                                                type="email"
+                                                name="email"
+                                                value="{{ old('email') }}"
+                                                class="form-control form-control-user @error('email') is-invalid @enderror"
+                                                id="email"
+                                                placeholder="Adresse e‑mail"
+                                                required
+                                                autofocus
+                                                autocomplete="username"
+                                            >
+                                            @error('email')
+                                                <div class="invalid-feedback d-block">
+                                                    {{ $message }}
+                                                </div>
+                                            @enderror
+                                        </div>
+
+                                        <button type="submit" class="btn btn-primary btn-user btn-block">
+                                            Envoyer le lien de réinitialisation
+                                        </button>
+
+                                        <hr>
+
+                                        <div class="text-center">
+                                            @if (Route::has('login'))
+                                                <a class="small" href="{{ route('login') }}">
+                                                    Retour à la connexion
+                                                </a>
+                                            @endif
+                                        </div>
+                                    </form>
+                                </div>
+                            </div> <!-- /col form -->
+                        </div>
+                    </div>
+                </div>
+
+            </div>
+        </div>
     </div>
 
-    <!-- Session Status -->
-    <x-auth-session-status class="mb-4" :status="session('status')" />
+    <!-- JS SB Admin 2 -->
+    <script src="{{ asset('sbadmin2/vendor/jquery/jquery.min.js') }}"></script>
+    <script src="{{ asset('sbadmin2/vendor/bootstrap/js/bootstrap.bundle.min.js') }}"></script>
+    <script src="{{ asset('sbadmin2/vendor/jquery-easing/jquery.easing.min.js') }}"></script>
+    <script src="{{ asset('sbadmin2/js/sb-admin-2.min.js') }}"></script>
 
-    <form method="POST" action="{{ route('password.email') }}">
-        @csrf
+</body>
+</html>
 
-        <!-- Email Address -->
-        <div>
-            <x-input-label for="email" :value="__('Email')" />
-            <x-text-input id="email" class="block mt-1 w-full" type="email" name="email" :value="old('email')" required autofocus />
-            <x-input-error :messages="$errors->get('email')" class="mt-2" />
-        </div>
 
-        <div class="flex items-center justify-end mt-4">
-            <x-primary-button>
-                {{ __('Email Password Reset Link') }}
-            </x-primary-button>
-        </div>
-    </form>
-</x-guest-layout>
