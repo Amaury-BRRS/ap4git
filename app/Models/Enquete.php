271e10f66d91a2ae3c4129457a08717623a49247
Amaury BRRS <amaury.brs70@outlook.com>
2026-03-13 11:54:22 +0100

    Update Enquete.php
    modif v2

--- app/Models/Enquete.php
+++ app/Models/Enquete.php
@@ -10,26 +10,30 @@ class Enquete extends Model
     /** @use HasFactory<\Database\Factories\EnqueteFactory> */
     use HasFactory;
 
-    public function question(){
-        return $this->hasMany(Question::class); 
+    public function questions()
+    {
+        return $this->HasMany(Question::class); 
     }
 
     // réponse au sens où on a répondu à l'enquête en générale, pas aux questions 
-    public function reponse(){
+    public function reponse()
+    {
         return $this->hasMany(Reponse::class); 
-    }
-
-    public function user(){
-        return $this->hasOne(User::class); 
-    }
-
-    public function participant(){
-        return $this->hasOne(Participant::class); 
     }
 
     public function echange()
     {
         return $this->hasMany(Echange::class); 
+    }
+
+    public function participant()
+    {
+        return $this->belongsToMany(Participant::class); 
+    }
+
+    public function user()
+    {
+        return $this->belongsTo(User::class, 'id_user');
     }
 
     protected $fillable = [
