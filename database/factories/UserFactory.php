92da8706c087ff5a3353fcec13b49acd5cd4de7e
Anis KEHAL <kehalanis25@gmail.com>
2026-03-13 11:27:56 +0100

    no message

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -7,6 +7,7 @@ use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Str;
 
 
+
 /**
  * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\User>
  */
@@ -24,14 +25,12 @@ class UserFactory extends Factory
      */
     public function definition(): array
     {
-        return [
-            'name' => fake()->name(),
-            'email' => fake()->unique()->safeEmail(),
-            'email_verified_at' => now(),
-            'password' => static::$password ??= Hash::make('password'),
-            'type_user' => 'user',
-            'remember_token' => Str::random(10),
-        ];
+       return [
+    'name' => $this->faker->name(),
+    'email' => $this->faker->unique()->safeEmail(),
+    'password' => bcrypt('password'),
+    'type_user' => $this->faker->randomElement(['user', 'admin', 'superadmin']),
+    ];
     }
 
     /**
