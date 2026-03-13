4bf2cafeb90da1aa6932e258e52c59b30a2b164e
Ryan <frisepoulet001@gmail.com>
2026-03-13 08:16:49 +0100

    Modification de la page etablissement

--- resources/views/superadmin/index.blade.php
+++ resources/views/superadmin/index.blade.php
@@ -5,7 +5,7 @@
     <p>Vous pouvez gérer les établissements et les formations depuis cet espace.</p>
 </div>
 <div class="superadmin-actions" style="display: flex; justify-content: center; gap: 20px; margin-top: 30px;">
-    <a href="{{ route('superadmin.etablissement.index') }}" class="btn btn-primary">Gérer les établissements</a>
-    <a href="#" class="btn btn-secondary">Gérer les formations</a>
+    <a href="{{ route('superadmin.etablissement.index') }}" class="btn btn-primary">Voir les établissements</a>
+    <a href="#" class="btn btn-secondary">Voir les formations</a>
 </div>
 @endsection
