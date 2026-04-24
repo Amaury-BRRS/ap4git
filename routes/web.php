4efb6ff4310eec84950d2edfc5dd89706b0bd98d
Ryan <frisepoulet001@gmail.com>
2026-04-24 11:24:11 +0200

    ajout route liste etablissement

--- routes/web.php
+++ routes/web.php
@@ -44,7 +44,7 @@ Route::middleware('is_super_admin')->group(function () {
 Route::middleware('is_admin')->group(function () {
 });
 
-// Route::get('/liste', [EtablissementController::class, 'index'])->name('superadmin.etablissement.index');
+Route::get('/liste', [EtablissementController::class, 'index'])->name('superadmin.etablissement.index');
 Route::get('/etablissement/create', [EtablissementController::class, 'create'])->name('superadmin.etablissement.create');
 Route::post('/etablissement', [EtablissementController::class, 'store'])->name('superadmin.etablissement.store');
 Route::get('/index', [SuperAdminController::class, 'index'])->name('superadmin.index');
